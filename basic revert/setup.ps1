7766cf8c84d1fed79cb165b071585ddec72f4df9
Suhas J <suhas.j@zemosolabs.com>
2021-07-07 15:27:07 +0530

    revert exaple

---
+++ basic revert/setup.ps1
@@ -0,0 +1,17 @@
+git status
+
+git log
+
+#modifies "revert example.txt" file
+
+git add -A
+
+git commit -m ""modified revert file"
+
+git log
+
+#copies last commit id
+
+git revert 91e1d8ef1ee4a4e3cd1e7c895c98df03b54efcc
+
+#reverts to older commit
