acc2dbb8ddc12035dc348a35a410a30b1ec1416c
Suhas J <suhas.j@zemosolabs.com>
2021-07-07 13:30:00 +0530

    three waymerge

---
+++ 3way merge/setup.ps1
@@ -0,0 +1,26 @@
+git branch greeting
+
+#modifing greeting
+
+git status
+
+git add greeting.txt
+
+git commit -m "3way merge"
+
+git checkout master
+
+#created readme.txt
+
+git add -A
+
+git log --oneline --graph --all
+
+git diff mybranch master
+
+git merge mybranch
+
+git log --online --graph --all
+
+
+# 3 way merge has occured 
