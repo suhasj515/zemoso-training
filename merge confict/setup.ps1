6cb2f325db31290fa6f2953d739358f483f33301
Suhas J <suhas.j@zemosolabs.com>
2021-07-07 14:05:07 +0530

    conflict resolved

---
+++ merge confict/setup.ps1
@@ -0,0 +1,21 @@
+# CReated two css files with modification done on Body element
+
+
+git add conflict.css
+git commit -m "conflit merge"
+
+git checkout master
+
+git merge mybranch
+
+# conflict arrises
+
+git pull origin master
+
+# manually made changes to conflict.css
+
+git add conflict.css
+
+git commit -m "conflict resolved"
+
+git merge mybranch
