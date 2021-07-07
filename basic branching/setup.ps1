3778527f8ec9610f450be5f844340ba6de74e56a
Suhas J <suhas.j@zemosolabs.com>
2021-07-07 12:31:38 +0530

    branching

---
+++ basic branching/setup.ps1
@@ -0,0 +1,28 @@
+git branch
+
+git branch mybranch
+
+git branch
+
+git checkout mybranch
+git status
+git checkout master
+git status
+
+git checkout mybranch
+git add -A
+
+git commit -m "basic branching"
+
+git log --oneline --graph
+
+git checkout master
+git commit origin master
+
+git log --oneline --graph --all
+
+git checkout mybranch
+
+git diff mybranch master
+
+
