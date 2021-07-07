ed012d0fb6da60ffa82d896f786dd604780fd1db
Suhas J <suhas.j@zemosolabs.com>
2021-07-07 11:19:59 +0530

    basic commits

---
+++ basic commits/setup.ps1
@@ -0,0 +1,15 @@
+git status
+git log
+#created file
+git status
+git add f1.txt
+git status
+git commit -m "basic commits"
+git status
+
+#changed content of file
+
+git status
+got commit -m "content changed"
+git status
+git log
